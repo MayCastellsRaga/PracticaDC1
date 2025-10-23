f640817694e5b281a030527257526946308d170e
MayCastellsRaga <mcr69@alumnes.udl.cat>
2025-10-23 18:39:17 +0200

    data cleaner

--- cloud/ingestor/ingestor.py
+++ cloud/ingestor/ingestor.py
@@ -3,5 +3,5 @@ import paho.mqtt.subscribe as subscribe
 
 def on_message_print(client, userdata, message):
     print("%s %s" % (message.topic, message.payload))
-subscribe.callback(on_message_print, "#", hostname="mosquitto")
+subscribe.callback(on_message_print, "#", hostname="mosquitto_cloud")
 
