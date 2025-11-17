3511496c0c46d584eed544e2687891b9d1898a16
MayCastellsRaga <mcr69@alumnes.udl.cat>
2025-11-17 17:37:27 +0100

    pls git let me pull

--- cloud/ingestor/ingestor.py
+++ cloud/ingestor/ingestor.py
@@ -1,7 +1,12 @@
 import paho.mqtt.subscribe as subscribe
+import paho.mqtt.client as mqtt
 
 
 def on_message_print(client, userdata, message):
     print("%s %s" % (message.topic, message.payload))
+    cloud_client = mqtt.Client()
+    cloud_client.connect("localhost", 1884, 60)
+    cloud_client.publish("raw_data", message.payload)
+    cloud_client.disconnect()
 subscribe.callback(on_message_print, "#", hostname="mosquitto_cloud")
 
