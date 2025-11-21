a8dd2f64d791b311530d5f5e012c9b7d6a101587
MayCastellsRaga <mcr69@alumnes.udl.cat>
2025-11-21 20:24:44 +0100

    p3 is finally working

--- cloud/kafka/DataCleaner.py
+++ cloud/kafka/DataCleaner.py
@@ -1,31 +1,31 @@
-from json import dumps, loads
-from time import sleep
-from kafka import KafkaProducer
-from kafka import KafkaConsumer
-import logging
-
-logging.basicConfig(level=logging.INFO)
-
-my_consumer = KafkaConsumer(
-    'raw_data',
-    bootstrap_servers=['localhost: 1884'],
-    auto_offset_reset='latest',
-    enable_auto_commit=True,
-    group_id='raw_group',
-    value_deserializer=lambda x: loads(x.decode('utf-8'))
-)
-my_producer = KafkaProducer(
-    'clean_data',
-    bootstrap_servers=['localhost:1884'],
-    enable_auto_commit=True,
-    group_id='clean_group',
-    value_deserializer=lambda x: loads(x.decode('utf-8'))
-)
-
-print("starting")
-for message in my_consumer:
-
-    message = message.value
-    if message != 100:
-        my_producer.send('clean_data',message)
-        print(f"{message} processed")
+#from json import dumps, loads
+#from time import sleep
+#from kafka import KafkaProducer
+#from kafka import KafkaConsumer
+#import logging
+#
+#logging.basicConfig(level=logging.INFO)
+#
+#my_consumer = KafkaConsumer(
+#    'raw_data',
+#    bootstrap_servers=['localhost: 1884'],
+#    auto_offset_reset='latest',
+#    enable_auto_commit=True,
+#    group_id='raw_group',
+#    value_deserializer=lambda x: loads(x.decode('utf-8'))
+#)
+#my_producer = KafkaProducer(
+#    'clean_data',
+#    bootstrap_servers=['localhost:1884'],
+#    enable_auto_commit=True,
+#    group_id='clean_group',
+#    value_deserializer=lambda x: loads(x.decode('utf-8'))
+#)
+#
+#print("starting")
+#for message in my_consumer:
+#
+#    message = message.value
+#    if message != 100:
+#        my_producer.send('clean_data',message)
+#        print(f"{message} processed")
--- user/user/gateway/gateway.py
+++ user/user/gateway/gateway.py
@@ -3,15 +3,18 @@ import paho.mqtt.subscribe as subscribe
 import paho.mqtt.client as mqtt
 import json
 import os
-
-cloud_client = mqtt.Client()
-cloud_client.connect("mosquitto_cloud", 1884, 60)
+import time
+import threading
+mqtt_host = os.getenv("MQTT_HOST")
 
 
 project_name = os.environ.get("PROJECT_NAME", "default")
 print("Project name:", project_name)
 
+
 def on_message_print(client, userdata, message):
+    cloud_client = mqtt.Client()
+    cloud_client.connect("mosquitto_cloud", 1884, 60)
     print("%s %s" % (message.topic, message.payload))
     # Publish to cloud MQTT
     try:
@@ -28,8 +31,46 @@ def on_message_print(client, userdata, message):
     cloud_client.publish("main", json.dumps(data))
     print("Published to cloud:", data)
     #cloud_client.disconnect()
+def on_message_sensor(client,userdata,message):
+    user_client = mqtt.Client()
+    user_client.connect(mqtt_host, 1883, 60)
+    print("%s %s" % (message.topic, message.payload))
+    try:
+        payload = json.loads(message.payload.decode("utf-8"))
+    except Exception as e:
+        print(f"error decoding payload: {e}")
+        return
+    print("Payload value:",payload)
+    print("message value:",message.payload)
+    data = {
+        "user":project_name,
+        "room": payload.get("room",""),
+        "timpestamp": payload.get("timestamp",""),
+        "value": payload.get("value","")
+    }
+    user_client.publish("user2/heatpump",json.dumps(data))
+    print("Published to controler:", data)
 
 
+#///// Will handle messages from controler to the gateway and send it to the cloud /////
 
-subscribe.callback(on_message_print, "+/temperature", hostname="mosquitto", port=1883)
+mosquito_client=mqtt.Client()
+mosquito_client.connect(mqtt_host,1883,60)
+mosquito_client.loop_start()
+mosquito_client.subscribe("+/temperature")
+mosquito_client.on_message = on_message_print
+#////////////////////////////////////////////
 
+#///// Will handle messages from kafka to the gateway and then send them to the controler/////
+
+kafka_client =mqtt.Client()
+kafka_client.connect("mosquitto_cloud",1884,60)
+kafka_client.subscribe(f"{project_name}/kafka_heatpump",)
+kafka_client.on_message = on_message_sensor
+kafka_client.loop_start()
+
+#////////////////////////////////////////////
+
+
+while True:
+    time.sleep(1)
